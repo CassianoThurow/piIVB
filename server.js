1600d5c44e87818ac70c4f5845e3f0660afa125a
Cassiano Thurow <cassiano.thurow@gmail.com>
2020-10-27 14:51:15 -0300

    finish CRUD

--- server.js
+++ server.js
@@ -16,26 +16,55 @@ const writeFile = (clients) => {
   fs.writeFileSync('./clients.json', updateFile)
 }
 
-
-router.get('/list', function (req, res) {
+router.get('/list',  (req, res) => {
   const clients = readFile();
   res.send(clients)
 })
 
-router.post('/create', function(req, res){
+router.post('/create', (req, res) =>{
   const newClients = readFile();
-  const {id, name, adress, cep, birth, phone} = req.body;
+  const {name, adress, cep, birth, phone} = req.body;
+  const id = Math.random().toString(32).substr(2, 9)
   newClients.push({ id, name, adress, cep, birth, phone})
   writeFile(newClients)
   res.send(newClients);
 })
 
-router.delete('/delete', function(req, res){
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
 
+  const {name, adress, cep, birth, phone} = req.body;
+
+  const currentClient = readFile();
+
+  const selectedItem = currentClient.findIndex((item)=> item.id === id)
+
+  const {id: cId, name: cName, adress: cAdress, cep: cCep, birth: cBirth, phone: cPhone} = currentClient[selectedItem];
+
+  const newObjects = {
+    id: cId,
+    name: name ? name: cName,
+    adress: adress ? adress: cAdress,
+    cep: cep ? cep: cCep,
+    birth: birth ? birth: cBirth,
+    phone: phone ? phone: cPhone,
+  };
+
+  currentClient[selectedItem] = newObjects
+
+  writeFile(currentClient);
+
+  res.send(newObjects)
 
 })
 
-
-
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const currentClient = readFile()
+  const selectedItem = currentClient.findIndex((item)=> item.id === id)
+  currentClient.splice(selectedItem, 1)
+  writeFile(currentClient)
+  res.send("Cliente Excluido !")
+})
 
 app.listen(3000);
