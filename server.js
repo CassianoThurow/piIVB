923f42bd33422f965476e283f2f55ffca9d8c630
Cassiano Thurow <cassiano.thurow@gmail.com>
2020-10-26 21:58:24 -0300

    create routes list/create

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 const express = require('express')
 const app = express()
-const clients = require('./clients.json');
+const router = express.Router()
+const fs = require('fs');
+
+app.use(express.json());
+app.use(router);
+
+const readFile = () => {
+  const clients = fs.readFileSync('./clients.json');
+  return (JSON.parse(clients))
+}
+
+const writeFile = (clients) => {
+  const updateFile = JSON.stringify(clients);
+  fs.writeFileSync('./clients.json', updateFile)
+}
 
 
-app.get('/', function (req, res) {
-  res.json(clients)
+router.get('/list', function (req, res) {
+  const clients = readFile();
+  res.send(clients)
 })
 
-app.listen(3000)
+router.post('/create', function(req, res){
+  const newClients = readFile();
+  const {id, name, adress, cep, birth, phone} = req.body;
+  newClients.push({ id, name, adress, cep, birth, phone})
+  writeFile(newClients)
+  res.send(newClients);
+})
+
+router.delete('/delete', function(req, res){
+
+
+})
+
+
+
+
+app.listen(3000);
